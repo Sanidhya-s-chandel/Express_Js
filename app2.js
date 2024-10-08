12d1171b2dfb5c835936c410b9312e0986802927
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-08 18:13:21 +0530

    Methods

---
+++ app2.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+
+app.use(express.static("public")) // to use Post request use this line
+
+// get request is a basic request from the browser through URL, 8192 bytes limit
+
+app.get("/",(req,res)=>{
+    console.log('its a get request');
+    res.send("Hello...........")
+})
+
+// post request is used to sent sensitive information to the server or the data large in size
+
+app.post("/post",(req,res)=>{
+    console.log('its a Post request');
+    res.send("Hello........... POST!!")
+})
+
+// put request is use to updating the resources
+
+app.put("/put",(req,res)=>{
+    console.log('its a Put request');
+    res.send("Hello........... PUT!!")
+})
+
+// delete is used to handel delete request for the specified path
+
+app.delete('/delete', (req, res) => { 
+    console.log("route Delted");
+    res.send("DELETE Request Called") 
+}) 
+
+app.get("/api",(req,res)=>{
+    res.json({
+        a:1,b:2,c:3,d:4,
+        name:["Shanu","jaggu"]
+    })
+})
+
+app.listen(3000)
